cfcd86100cb1192ec64a7ef85d77a0f410ca93ca
Peinda FALL <71086525+fallpeindafall@users.noreply.github.com>
2021-01-10 17:38:09 +0000

    latest version

---
+++ PlusGrandCommunDiviseur/AlgorithmePGCD_PF.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace PlusGrandCommunDiviseur
+{
+    static class AlgorithmePGCD_PF
+    {
+     
+        static public int TrouverPGCD(int a, int b, out long dureeEuclide)
+        {
+            // Initialiser chrono
+            dureeEuclide = 0;
+            Stopwatch chrono = new Stopwatch();
+            chrono.Start();
+
+            // Si le premier nombre est égal à zéro, renvoyer le second
+            if (a == 0) return b;
+          
+           
+           
+            while (b != 0)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+
+            chrono.Stop();
+            dureeEuclide = chrono.ElapsedTicks;
+            return a;
+        }
+
+      /*  static public int TrouverPGCD3(int a, int b,int c)
+        {
+
+            int x = TrouverPGCD(a, b);
+            return TrouverPGCD(x, c);
+           
+        }
+        static public int TrouverPGCD4(int a, int b, int c,int e)
+        {
+
+            int x = TrouverPGCD3(a, b,c);
+            return TrouverPGCD(x, e);
+
+        }
+
+        static public int TrouverPGCD5(int a, int b, int c, int e, int f)
+        {
+
+            int x = TrouverPGCD4(a, b, c, e);
+            return TrouverPGCD(x, f);
+
+        }*/
+
+
+        //TASK 3
+        static public int TrouverPGCDStein(int u,int v, out long dureeStein)
+        {
+            // Initialiser chrono
+            dureeStein = 0;
+            Stopwatch chrono = new Stopwatch();
+            chrono.Start();
+
+            int k;
+
+            // Etape 1.
+            // pgcd(0, v) = v, car tout  divise zéro, 
+            // et v est le plus grand nombre qui divise v. 
+            // Idem, pgcd(u, 0) = u. pgcd(0, 0) n'est pas habituellement défini 
+            // mais il est commode de poser pgcd(0, 0) = 0.
+            if (u == 0 || v == 0)
+                return u | v;
+
+            // Etape 2.
+            // Si u et v sont tous deux pairs, alors pgcd(u, v) = 2•pgcd(u/2, v/2), 
+            // car 2 est un  diviseur commun. 
+            for (k = 0; ((u | v) & 1) == 0; ++k)
+            {
+                u >>= 1;
+                v >>= 1;
+            }
+
+            // Etape 3.
+            // Si u est pair et v est impair, alors pgcd(u, v) = pgcd(u/2, v), 
+            // car 2 n'est pas un  diviseur commun. 
+            // Idem, si u est impair et v pair, 
+            // alors pgcd(u, v) = pgcd(u, v/2). 
+            while ((u & 1) == 0)
+                u >>= 1;
+
+            // Etape 4.
+            // If u et v sont tous impairs, et u ≥ v, 
+            // alors pgcd(u, v) = pgcd((u − v)/2, v). 
+            // If tous sont impairs et u < v, alors pgcd(u, v) = pgcd((v − u)/2, u). 
+            // Voila les combinaisons en une Etape de   
+            //  l'algorithme Euclidien simple, 
+            // qui utilise la soustraction à chaque Etape, et une application 
+            // de l'Etape 3 ci-dessus. 
+            // La division par 2 tombe sur un entier car la 
+            // difference de deux nombres impairs  est paire.
+            do
+            {
+                while ((v & 1) == 0)  // Boucle x
+                    v >>= 1;
+
+                //  u et v sont tous impairs, alors diff(u, v) est pair.
+                //   Soit u = min(u, v) et v = diff(u, v)/2. 
+                if (u < v)
+                {
+                    v -= u;
+                }
+                else
+                {
+                    int diff = u - v;
+                    u = v;
+                    v = diff;
+                }
+                v >>= 1;
+                // Etape 5.
+                // Répéter les Etapes 3–4 jusqu'à ce que u = v, ou (Etape supplémentaire) 
+                // jusqu'à ce que u = 0.
+                // Quelque soit le cas, le résultat est (2^k) * v, où k est 
+                // le nombre de facteurs communs  de 2 trouvé à l'Etape 2. 
+            } while (v != 0);
+
+            u <<= k;
+
+            chrono.Stop();
+            dureeStein = chrono.ElapsedTicks;
+            return u;
+        }
+    }
+
+}
---
+++ PlusGrandCommunDiviseur/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace PlusGrandCommunDiviseur
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        private void Calculer_PGCD_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (!LireEntierPositifZoneTexte(entier1, out int premierNombre)) return;
+            if (!LireEntierPositifZoneTexte(entier2, out int secondNombre)) return;
+            if (!LireEntierPositifZoneTexte(entier3, out int troisiemeNombre)) return;
+            if (!LireEntierPositifZoneTexte(entier4, out int quatriemeNombre)) return;
+            if (!LireEntierPositifZoneTexte(entier5, out int cinquiemeNombre)) return;
+
+
+            // Afficher les résultats 
+            if (sender == trouverPGCD) // Euclide et Stein pour 2 entiers
+            {
+                // Appeler la méthode TrouverPGCD  et afficher le résultat
+
+                this.resultatEuclide.Content = String.Format("Euclide: {0}, Durée (en ticks): {1}", AlgorithmePGCD_PF.TrouverPGCD(premierNombre, secondNombre, out long dureeEuclide), dureeEuclide);
+                
+                // Appeler la méthode TrouverPGCDStein et afficher le résultat
+                
+                this.resultatStein.Content = String.Format("Stein: {0}, Durée (en ticks): {1}", AlgorithmePGCD_PF.TrouverPGCDStein(premierNombre, secondNombre, out long dureeStein), dureeStein);
+
+            }
+            /* if (sender == trouverPGCD3) // Euclide pour 3 entiers
+             {
+                 // Appeler la méthode TrouverPGCD3  et afficher le résultat
+
+                 this.resultatEuclide.Content = String.Format("Resultats: {0}", AlgorithmePGCD_PF.TrouverPGCD3(premierNombre, secondNombre, troisiemeNombre));
+             }
+
+             if (sender == trouverPGCD4) // Euclide pour 4 entiers
+             {
+
+                 // Appeler la méthode TrouverPGCD4   et afficher le résultat
+
+                 this.resultatEuclide.Content = String.Format("Resultats: {0}", AlgorithmePGCD_PF.TrouverPGCD4(premierNombre, secondNombre, troisiemeNombre,quatriemeNombre));
+             }
+
+
+             if (sender == trouverPGCD5) // Euclide pour 5 entiers
+             {
+
+                 // Appeler la méthode TrouverPGCD5  et afficher le résultat
+
+                 this.resultatEuclide.Content = String.Format("Resultats: {0}",AlgorithmePGCD_PF.TrouverPGCD5(premierNombre, secondNombre,troisiemeNombre, quatriemeNombre,cinquiemeNombre));
+             }*/
+
+        }
+
+        /// <summary>
+        /// Lire un entier postif  à partir d'une zone de texte
+        /// Affiche une boite de message avec les données si le texte ne peut pas être parsé.
+        /// </summary>
+        /// <param name="zoneTexte">Textzone de texte à lire</param>
+        /// <param name="i">entier Postif  (paramètre out)</param>
+        /// <returns>True si succès, false sinon</returns>
+        private bool LireEntierPositifZoneTexte(TextBox zoneTexte, out int i)
+        {
+            i = -1;
+            if (int.TryParse(zoneTexte.Text, out i))
+            {
+                if (i >= 0) return true;
+            }
+            MessageBox.Show("entrez un nombre positif svp: " );
+            return false;
+        }
+
+      
+    }
+}
